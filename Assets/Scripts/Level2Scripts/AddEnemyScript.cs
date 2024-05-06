9b2b5bb8960068f8e1b67bde94d2015185b78c51
Demurecrowd <demurecrowd@gmail.com>
2024-05-06 15:54:36 -0400

    Fixed Errors in Level 1 and 2 Added enemy appearing script
    Fixed all errors in levels 1 and 2, also added a script for the mainframe switch to make enemy 1 appear

---
+++ Assets/Scripts/Level2Scripts/AddEnemyScript.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AddEnemyScript : MonoBehaviour
+{
+  
+    public GameObject Enemy;
+    public GameObject genLeverInteraction;
+    public bool inTrig, oneTime, genOn;
+    
+    void Start()
+    {
+        Enemy.SetActive(false);
+    }
+     private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (!oneTime){
+                inTrig = true;
+            }
+            if (oneTime)
+            {
+                inTrig = false;
+            }
+        }
+        
+    }
+    void Update()
+    {
+        if (inTrig)
+        {
+            if(genLeverInteraction.GetComponent<LeverScript>().oneTime == true)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    Enemy.SetActive(true);
+
+                }
+            }
+        }
+    }
+
+}
